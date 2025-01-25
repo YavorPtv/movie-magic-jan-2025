3014e147d097a3195662e191c8eba370174d9db9
YavorPtv <yavorpetrakiev@gmail.com>
2025-01-25 13:06:24 +0200

    Parse form data on server

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', './src/views')
 
-app.use('/static', express.static('src/public'))
+app.use('/static', express.static('src/public'));
+app.use(express.urlencoded({extended: false})); // Learn express to parse data
 
 app.use(routes);
 
