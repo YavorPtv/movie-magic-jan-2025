e3ea5af45625e50f51cc4cde626d196c15523c87
YavorPtv <yavorpetrakiev@gmail.com>
2025-01-28 23:25:28 +0200

    Refactor home page to use database movies

--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -1,28 +1,26 @@
 import { v4 as uuid} from 'uuid';
-import movies from "../movies.js";
+// import movies from "../movies.js";
+import Movie from '../models/Movie.js';
 
 export default {
     getAll(filter = {}){
-        let result = movies;
 
-        if (filter.search){
-            result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()));
-        }
+        // if (filter.search){
+        //     result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()));
+        // }
 
-        if (filter.genre){
-            result = result.filter(movie => movie.genre.toLowerCase() === filter.genre);
-        }
+        // if (filter.genre){
+        //     result = result.filter(movie => movie.genre.toLowerCase() === filter.genre);
+        // }
 
-        if (filter.year){
-            result = result.filter(movie => movie.year === filter.year);
-        }
+        // if (filter.year){
+        //     result = result.filter(movie => movie.year === filter.year);
+        // }
 
-        return result;
+        return Movie.find({});
     },
-    findOne(movieId){
-        //TODO: if movie is missing?
-        const result = movies.find(movie => movie.id === movieId);
-
+    getOne(movieId){
+        const result = Movie.findById(movieId);
         return result;        
     },
     create(movieData){
