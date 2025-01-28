69e8eb621cdc756026ba4d72cbabb84589d75ede
YavorPtv <yavorpetrakiev@gmail.com>
2025-01-28 22:47:34 +0200

    Setup mongoose

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import express from 'express';
 import handlebars from 'express-handlebars'
+import mongoose from 'mongoose';
+
 import routes from './routes.js';
 import showRatingHelper from './helpers/rating-helper.js';
 
 const app = express();
 
+// db config
+try{
+    const uri = 'mongodb://localhost:27017/magic-movies-jan2025'
+    await mongoose.connect(uri);
+    console.log('DB connected successfully!')
+} catch(err){
+    console.log('Cannot connect to DB');
+    console.error(err.message);
+}
+
+// handlebars config
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
     helpers: {
@@ -14,9 +27,12 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', './src/views')
 
+// express config
 app.use('/static', express.static('src/public'));
 app.use(express.urlencoded({extended: false})); // Learn express to parse data
 
+// setup routes
 app.use(routes);
 
+// start server
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
