5159be7329c393d860c8508cb861f95fdca8e217
YavorPtv <yavorpetrakiev@gmail.com>
2025-02-04 20:38:59 +0200

    Show details buttons if user is creator

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -28,7 +28,9 @@ movieController.get('/:movieId/details', async (req, res) => {
     const movieId = req.params.movieId;
     const movie = await movieService.getOneWithCasts(movieId);
     
-    res.render('movie/details', { movie });
+    const isCreator = movie.creator && movie.creator.toString() === req.user?.id;
+
+    res.render('movie/details', { movie, isCreator });
 })
 
 movieController.get('/:movieId/attach-cast', async (req, res) => {
