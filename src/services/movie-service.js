0adc8facf48affc3f4baefb63a10bdfab45035aa
YavorPtv <yavorpetrakiev@gmail.com>
2025-02-07 19:09:52 +0200

    Add tempData middleware

--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -43,7 +43,7 @@ export default {
         return Movie.findByIdAndDelete(movieId);
     },
     update(movieId, movieData){
-        return Movie.findByIdAndUpdate(movieId, movieData);
+        return Movie.findByIdAndUpdate(movieId, movieData, { runValidators: true });
     }
 }
 
